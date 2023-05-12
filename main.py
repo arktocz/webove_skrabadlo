9470f8b9cba2fb8f2c3a7b2f7fcd16c247cd9af7
arktocz <arktocz@gmail.com>
2023-05-12 07:33:28 +0200

    done

--- main.py
+++ main.py
@@ -3,37 +3,37 @@ from ast import For
 from optparse import Values
 from playwright.sync_api import Playwright, sync_playwright, expect
 import re
+import json
 
 
 #SCRAPE JEDNE PUBLIKACE
 def scrape_publication(publication_page):
-    #publication_page.get_by_role("link", name="Leadership a stress").click()
     #name
     name_locator = publication_page.locator('tr:has-text("Název v původním jazyce") td:nth-child(2)')
     name=name_locator.inner_text()
-    print(name)
+    #print(name)
     #place
     place_locator = publication_page.locator('tr:has-text("Místo vydání") td:nth-child(2)')
     place=place_locator.inner_text()
-    print(place)
+    #print(place)
     #published date
     published_date_locator = publication_page.locator('tr:has-text("Rok uplatnění") td:nth-child(2)')
     published_date=published_date_locator.inner_text()
-    print(published_date)
+    #print(published_date)
     #publication type
     publication_type_locator = publication_page.locator('tr:has-text("Druh výsledku") td:nth-child(2)')
     publication_type=publication_type_locator.inner_text()
-    print(publication_type)
+    #print(publication_type)
     #hyperodkaz
     second_table = publication_page.locator('table').nth(1)
     eleventh_tr_second_col = second_table.locator('tr:nth-child(11) td:nth-child(2)')
     reference= eleventh_tr_second_col.inner_text()
-    print(reference)
+    #print(reference)
     #isbn
     third_table = publication_page.locator('table').nth(2)
     second_tr_sec_col = third_table.locator('tr:nth-child(2) td:nth-child(2)')
     isbn=second_tr_sec_col.inner_text()
-    print(isbn)
+    #print(isbn)
     #amount of authors check
     author_table = publication_page.locator('tbody').nth(0)
     tr_count = author_table.locator('tr').count()
@@ -43,12 +43,15 @@ def scrape_publication(publication_page):
         string='tr:nth-child('+str(i)+') td:nth-child(2)'
         author = author_table.locator(string).inner_text()
         list_of_authors.append(author)
-    print(list_of_authors)
+    publication_list=[name, place, published_date, publication_type, reference, isbn, list_of_authors]
+    #print(publication_list)
+    return publication_list
 
 #LISTOVÁNÍ PUBLIKACEMI 
 def individual_publication_view(page, browser, max_publication): 
     all_publication_table = page.locator('tbody').nth(0)
     publication_context = browser.new_context()
+    publication_dict = {}
     for x in range(1, max_publication):
         locator='tr:nth-child('+str(x)+') td:nth-child(3) li:nth-child(1)'
         #print(locator)
@@ -61,10 +64,15 @@ def individual_publication_view(page, browser, max_publication):
         publication_page = publication_context.new_page()
         publication_page.goto(complete_url)
         #scrape publikace
-        scrape_publication(publication_page)
+        publication_list=scrape_publication(publication_page)
+        key = f"publication{x}"
+        publication_dict[key]=publication_list
+        #print(publication_list)
         publication_page.wait_for_timeout(500)
         publication_page.close()
     publication_context.close
+    #print(publication_dict)
+    return publication_dict
 
 ##NAČTENÍ KOMPLETNÍHO LISTU PUBLIKACÍ KE SCRAPU-done
 def load_all_publications(page):
@@ -108,8 +116,13 @@ def run(playwright: Playwright) -> None:
     #NACTENI VSECH PUBLIKACI
     load_all_publications(page)
 
-    #listovani publikacemi-scrape
-    individual_publication_view(page, browser, 10)
+    #listovani publikacemi+scrape
+    publication_dict=individual_publication_view(page, browser, 10)
+    print(publication_dict)
+
+    #uloženi do jsonu
+    with open("data.json", "w", encoding="utf-8") as f:
+        json.dump(publication_dict, f, ensure_ascii=False)
     
 
 
